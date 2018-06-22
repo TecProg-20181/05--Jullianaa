7728ed3807060f0c884c7d57513bcc90cdf5a42b
jullianaa <julliana.coutoalmeida@gmail.com>
2018-06-22 20:12:57 -0300

    Adding test bytes readable qith block equals 1

--- diskspace/test_diskspace.py
+++ diskspace/test_diskspace.py
@@ -2,10 +2,16 @@ import unittest
 from diskspace import *
 
 class Diskspace_test(unittest.TestCase):
+    
     def test_bytes_readable(self):
         blocks = 0
         result = bytes_to_readable(blocks)
         self.assertEqual('0.00B', result) 
 
+    def test_bytes_readable(self):
+        blocks = 1
+        result = bytes_to_readable(blocks)
+        self.assertEqual('512.00B', result) 
+        
 if __name__ == '__main__':
     unittest.main()
